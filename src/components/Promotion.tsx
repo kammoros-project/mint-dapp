d41671b3476917ad4f12ecdfce7b266f0368b738
Bob <heycryptobob@gmail.com>
2022-10-10 20:59:13 +0300

    lint fix

--- src/components/Promotion.tsx
+++ src/components/Promotion.tsx
@@ -1,14 +1,6 @@
 import Container from "./layout/Container"
 import Countdown from 'react-countdown';
 
-// const data = [
-//     { amount: 100, value: 500, bonus: 70000 },
-//     { amount: 167, value: 300, bonus: 40000 },
-//     { amount: 250, value: 200, bonus: 28000 },
-//     { amount: 500, value: 100, bonus: 14000 },
-//     { amount: 1000, value: 50, bonus: 7000 },
-// ]
-
 function Promotion() {
 
     return (
@@ -31,30 +23,3 @@ function Promotion() {
 }
 
 export default Promotion
-
-{/* <h2 className="text-xl font-bold uppercase">Pre-Launch Bonus</h2>
-                            <p className="my-4">Mint your NFTs before the launch and receive a <span className="underline">bonus airdrop</span> of tokens.</p>
-                            <p>
-                                <span className="mr-2">Time until launch:</span>
-                                
-                            </p> */}
-{/* <div>
-                            <table>
-                                <thead>
-                                    <tr>
-                                        <td className="border-2 border-slate-800 py-2 px-4 text-right bg-slate-800/40">Quantity</td>
-                                        <td className="border-2 border-slate-800 py-2 px-4 text-right bg-slate-800/40">Value (BUSD)</td>
-                                        <td className="border-2 border-slate-800 py-2 px-4 text-right bg-slate-800/40">Airdrop (Bonus)</td>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    {data.map((d, k) => (
-                                        <tr key={k}>
-                                            <td className="border-2 border-slate-800 py-2 px-4 text-right">{d.amount}</td>
-                                            <td className="border-2 border-slate-800 py-2 px-4 text-right">{d.value}</td>
-                                            <td className="border-2 border-slate-800 py-2 px-4 text-right">{d.bonus}</td>
-                                        </tr>
-                                    ))}
-                                </tbody>
-                            </table>
-                        </div> */}
--- src/components/nft/NFTDrop.tsx
+++ src/components/nft/NFTDrop.tsx
@@ -1,4 +1,4 @@
-import { useActiveClaimCondition, useClaimedNFTSupply, useContract, useContractMetadata, useNFT, useUnclaimedNFTSupply, Web3Button } from "@thirdweb-dev/react"
+import { useActiveClaimCondition, useClaimedNFTSupply, useContract, useContractMetadata, useUnclaimedNFTSupply, Web3Button } from "@thirdweb-dev/react"
 import { BigNumber } from "ethers"
 import { formatUnits } from "ethers/lib/utils"
 import { useEffect, useState } from "react"
@@ -39,14 +39,13 @@ function NFTDrop({ contractAddress, bonus }: INFTDrop) {
     return (
         <div className="flex flex-col gap-8 border-2 md:border-0 px-2 pt-2 pb-8 md:p-0 rounded-3xl">
             <div className="group relative">
-                <img src={contractMetadata?.image} alt={`${contractMetadata?.name} preview image`} className="rounded-2xl w-fit" />
+                <img src={contractMetadata?.image} alt={`${contractMetadata?.name} preview`} className="rounded-2xl w-fit" />
                 <div className="absolute top-0 left-0 w-full h-full bg-transparent text-white p-4 flex items-end">
                     <div className="text-xs uppercase font-semibold p-2 bg-slate-700 w-fit rounded border-2">{bonus} KMT Bonus</div>
                 </div>
                 <div className="absolute top-0 left-0 w-full h-0 bg-slate-800 opacity-0 group-hover:h-full group-hover:opacity-90 duration-500 text-white p-4">
                     <div className="flex flex-col justify-center items-center h-full gap-4">
                         <h2 className="text-xl font-bold">{contractMetadata?.name}</h2>
-                        {/* <div className="border rounded p-2 text-lg font-bold">{activeClaimCondition?.currencyMetadata.displayValue} {activeClaimCondition?.currencyMetadata.symbol}</div> */}
                         <div>
                             <span>{claimedSupply?.toNumber()}</span>
                             <span className="mx-2">/</span>
